5a47b990a61918a3fda60ed3faec0b70ac60333b
venkatgn <venkatadrin.g@gmail.com>
2015-05-21 23:42:04 +0530

    updated home page program with few statements

--- MyProj/src/test/java/P1/Home.java
+++ MyProj/src/test/java/P1/Home.java
@@ -20,6 +20,7 @@ public class Home
   {
 	  driver.manage().window().maximize();
 	  driver.get("http://www.yahoomail.com");
+	  System.out.println("opened website");
   }
   public void validate_HomeLinks() throws Exception
   {
